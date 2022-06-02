101346ef2bc96d18549fe341fb7c242b47b29c06
lunagp <lunagp03@gmail.com>
2022-06-02 09:40:14 -0500

    recycler sin bd

---
+++ app/src/main/java/com/example/deplegable/model/Publicaciones.java
@@ -0,0 +1,46 @@
+package com.example.deplegable.model;
+
+public class Publicaciones {
+
+    private int foto;
+    private String descripcion;
+    private String ubicacion;
+
+
+    public Publicaciones(int foto, String descripcion, String ubicacion) {
+        this.foto = foto;
+        this.descripcion = descripcion;
+        this.ubicacion = ubicacion;
+
+    }
+
+    public int getFoto() {
+        return foto;
+    }
+
+    public void setFoto(int foto) {
+        this.foto = foto;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getUbicacion() {
+        return ubicacion;
+    }
+
+    public void setUbicacion(String ubicacion) {
+        this.ubicacion = ubicacion;
+    }
+
+
+    @Override
+    public String toString() {
+        return  foto + ' ' + descripcion + ' ' + ubicacion;
+    }
+}
--- app/src/main/java/com/example/deplegable/ui/Cultura/CulturaFragment.java
+++ app/src/main/java/com/example/deplegable/ui/Cultura/CulturaFragment.java
@@ -7,18 +7,42 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.deplegable.Adaptadores.Adaptador;
+import com.example.deplegable.R;
 import com.example.deplegable.databinding.FragmentCulturaBinding;
 import com.example.deplegable.databinding.FragmentGalleryBinding;
+import com.example.deplegable.model.Publicaciones;
 import com.example.deplegable.ui.gallery.GalleryViewModel;
+
+import java.util.ArrayList;
 
 public class CulturaFragment extends Fragment {
 
-    private FragmentCulturaBinding binding;
+    Adaptador adaptador;
+    RecyclerView recyclerViewCul;
+    ArrayList<Publicaciones> listaPubli;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
+    @NonNull
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_cultura, container, false);
+        recyclerViewCul = view.findViewById(R.id.recyclerView);
+
+        listaPubli = new ArrayList<>();
+        //cargar la lista
+        cargarLista();
+        //mostrar datos
+        mostrarDatos();
+
+        return view;
+    }
+    /*public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         CulturaViewModel culturaViewModel =
                 new ViewModelProvider(this).get(CulturaViewModel.class);
@@ -29,12 +53,25 @@ public class CulturaFragment extends Fragment {
         final TextView textView = binding.textcultura;
         culturaViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
+    }*/
+    public void cargarLista(){
+        listaPubli.add(new Publicaciones(R.drawable.agregar,"sisisisiis","esta sera "));
+        listaPubli.add(new Publicaciones(R.drawable.bar,"sisisisiis","esta sera "));
+        listaPubli.add(new Publicaciones(R.drawable.casa,"sisisisiis","esta sera "));
+        listaPubli.add(new Publicaciones(R.drawable.club,"sisisisiis","esta sera "));
+        listaPubli.add(new Publicaciones(R.drawable.campestre,"Steven lo logramos","sena"));
     }
 
-    @Override
+    public void mostrarDatos(){
+        recyclerViewCul.setLayoutManager(new LinearLayoutManager(getContext()));
+        adaptador = new Adaptador(getContext(), listaPubli);
+        recyclerViewCul.setAdapter(adaptador);
+    }
+
+    /*@Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
-    }
+    }*/
 
 }
